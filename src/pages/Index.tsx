7c6dfef40bdad2fe1d7796f16c7f9e63cb72aa73
Amitay <amitay1@gmail.com>
2026-01-01 08:22:20 +0200

    v1.0.50

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1622,38 +1622,40 @@ const Index = () => {
             </div>
           </Tabs>
 
-          {/* 3D Viewer Panel - Below the form fields */}
-          <div className="hidden lg:block px-4 pb-4">
-            <Collapsible3DPanel
-              title="3D Part Viewer"
-              isOpen={viewer3DOpen}
-              onToggle={() => setViewer3DOpen(!viewer3DOpen)}
-            >
-              <ThreeDViewer
-                partType={currentData.inspectionSetup.partType || ""}
-                material={currentData.inspectionSetup.material as MaterialType || ""}
-                dimensions={{
-                  length: currentData.inspectionSetup.partLength || 100,
-                  width: currentData.inspectionSetup.partWidth || 50,
-                  thickness: currentData.inspectionSetup.partThickness || 10,
-                  diameter: currentData.inspectionSetup.diameter || 50,
-                  isHollow: currentData.inspectionSetup.isHollow,
-                  innerDiameter: currentData.inspectionSetup.innerDiameter,
-                  innerLength: currentData.inspectionSetup.innerLength,
-                  innerWidth: currentData.inspectionSetup.innerWidth,
-                  wallThickness: currentData.inspectionSetup.wallThickness,
-                  coneTopDiameter: currentData.inspectionSetup.coneTopDiameter,
-                  coneBottomDiameter: currentData.inspectionSetup.coneBottomDiameter,
-                  coneHeight: currentData.inspectionSetup.coneHeight,
-                }}
-                scanDirections={currentData.scanDetails.scanDetails.map(detail => ({
-                  direction: detail.scanningDirection,
-                  waveMode: detail.waveMode,
-                  isVisible: detail.isVisible || false
-                }))}
-              />
-            </Collapsible3DPanel>
-          </div>
+          {/* 3D Viewer Panel - Only visible on Setup tab */}
+          {activeTab === "setup" && (
+            <div className="hidden lg:block px-4 pb-4">
+              <Collapsible3DPanel
+                title="3D Part Viewer"
+                isOpen={viewer3DOpen}
+                onToggle={() => setViewer3DOpen(!viewer3DOpen)}
+              >
+                <ThreeDViewer
+                  partType={currentData.inspectionSetup.partType || ""}
+                  material={currentData.inspectionSetup.material as MaterialType || ""}
+                  dimensions={{
+                    length: currentData.inspectionSetup.partLength || 100,
+                    width: currentData.inspectionSetup.partWidth || 50,
+                    thickness: currentData.inspectionSetup.partThickness || 10,
+                    diameter: currentData.inspectionSetup.diameter || 50,
+                    isHollow: currentData.inspectionSetup.isHollow,
+                    innerDiameter: currentData.inspectionSetup.innerDiameter,
+                    innerLength: currentData.inspectionSetup.innerLength,
+                    innerWidth: currentData.inspectionSetup.innerWidth,
+                    wallThickness: currentData.inspectionSetup.wallThickness,
+                    coneTopDiameter: currentData.inspectionSetup.coneTopDiameter,
+                    coneBottomDiameter: currentData.inspectionSetup.coneBottomDiameter,
+                    coneHeight: currentData.inspectionSetup.coneHeight,
+                  }}
+                  scanDirections={currentData.scanDetails.scanDetails.map(detail => ({
+                    direction: detail.scanningDirection,
+                    waveMode: detail.waveMode,
+                    isVisible: detail.isVisible || false
+                  }))}
+                />
+              </Collapsible3DPanel>
+            </div>
+          )}
         </div>
       </div>
 
